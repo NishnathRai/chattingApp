93854be51ed1a1978b8a6bd2617c801b4bcb5f88
Nishnath Rai <nishnathnishu1122@gmail.com>
2024-11-17 13:45:24 +0530

    created user schema

--- backEnd/schemas/userSchema.js
+++ backEnd/schemas/userSchema.js
@@ -14,9 +14,54 @@ const userSchema = new mongoose.Schema({
           type : String ,
           validate : {
               validator :function (val){
-                return /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email) ;
+                return /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(val) ;
               },
               message : `Enter valid email id`
+          },
+          index:true,
+          unique:true
+      },
+      password : {
+          type : String ,
+          validate : {
+              validator : function(val){
+                 return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/.test(val);
+              },
+              message : `a minimum length of 6 characters and a mix of lowercase letters, uppercase letters digits, and special characters`,
           }
+      },
+      bio :{
+          type : String ,
+          validate : {
+            validator : function(val){
+                return  val.length>=10 && val.length<=100;
+            },
+            message : ` At least 10 characters and max 100 characters`
+          },
+          default : `The end is the Beginning 🚀`
+      },
+      status : {
+          type : String ,
+          validate : {
+              validator : function(val){
+                return val.index>=5 && val.length<=20;
+              },
+              message : ` At least 5 characters and max 20 characters`
+          },
+          default : `Together 🫶`
+      },
+      profilePicture : {
+          type : String,
+          validate : {
+              validator : function(val){
+                 return val.length>=1;
+              },
+              message : `Cannot push an empty Image`
+          },
+          default : `https://i.pinimg.com/736x/e4/96/d7/e496d7ccf54886ee88eaab0717c27250.jpg`
       }
 });
+
+const userModel = mongoose.model('User',userSchema);
+
+module.exports = userModel ;
