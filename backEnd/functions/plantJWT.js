505b64caf73b7a332a44a92f599179432e901578
Nishnath Rai <nishnathnishu1122@gmail.com>
2024-11-17 16:01:49 +0530

    sign and signup backend completed

--- backEnd/functions/plantJWT.js
+++ backEnd/functions/plantJWT.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 
 function plantJWT(req,res,next){
     const token =  jwt.sign( {_id:req.body._id} ,"Nishnath" ) ;
-    if(!req?.remember) res.cookie('token',token);
+    if(!req.body?.remember) res.cookie('token',token);
     else {
         res.cookie('token',token,{ maxAge: 100 * 365 * 24 * 60 * 60 * 1000 });
     }
--- backEnd/routs/user.js
+++ backEnd/routs/user.js
@@ -5,18 +5,23 @@ const Vaild = require("../functions/validate.js");
 const userModel = require("../schemas/userSchema.js");
 const bcrypt = require("bcrypt");
 const plantJWT = require('../functions/plantJWT.js');
+const isPsswordCorrect = require('../functions/isPasswordCorrect.js');
 ////
-userRouter.post("/signin",(req,res)=>{
+userRouter.post("/signin",async (req,res,next)=>{
     try{
        let email = req.body?.email;
        let password = req.body?.password;
        Vaild(0,email,password,"");
-       res.send(email + " " +password);
+       let user = await userModel.findOne({email}).exec();;
+       if(!user) throw Error("Please enter Vaild info");
+       await isPsswordCorrect(user,password);
+       req._id = user._id;
+       next();
     }
     catch(err){
-        res.status(400).send("moda " + err.message );
+        res.status(400).send(err.message );
     }
-});
+} , plantJWT ,(req,res) => { res.send("sign In Successfully") } );
 
 userRouter.post("/signup",async (req,res,next)=>{
     try{
