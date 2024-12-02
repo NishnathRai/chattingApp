4291c9ceed1725daf27b273e2c9830a53765dff7
Nishnath Rai <nishnathnishu1122@gmail.com>
2024-12-02 19:19:01 +0530

    added secure to cookies

--- backEnd/functions/plantJWT.js
+++ backEnd/functions/plantJWT.js
@@ -3,9 +3,9 @@ require("dotenv").config();
 
 function plantJWT(req,res,next){
     const token =  jwt.sign( {_id:req.body._id} , process.env.jwtSecreat ) ;
-    if(!req.body?.remember) res.cookie('token',String(token),{  httpOnly: true,secure: false, sameSite: 'lax',});
+    if(!req.body?.remember) res.cookie('token',String(token),{  httpOnly: true,secure: true, sameSite: 'lax',});
     else {
-        res.cookie('token',String(token),{  httpOnly: true,secure: false,  sameSite: 'lax',maxAge: 100 * 365 * 24 * 60 * 60 * 1000 });
+        res.cookie('token',String(token),{  httpOnly: true,secure: true,  sameSite: 'lax',maxAge: 100 * 365 * 24 * 60 * 60 * 1000 });
     }
     next();
 }
