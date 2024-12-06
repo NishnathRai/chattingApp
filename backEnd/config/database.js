c05518137de6cc7f148f72884b7fb270895f0f9c
Nishnath Rai <nishnathnishu1122@gmail.com>
2024-12-06 10:29:52 +0530

    deleated every thing from main

--- backEnd/config/database.js
+++
@@ -1,9 +0,0 @@
-const db_URL = "mongodb://localhost:27017/Together";
-const mongoose = require("mongoose");
-
-async function connect(){
-    await mongoose.connect(db_URL);
-}
-
-module.exports = connect ;
-
--- backEnd/functions/isPasswordCorrect.js
+++
@@ -1,9 +0,0 @@
-const bcrypt = require("bcrypt");
-
-async function isPsswordCorrect(user,inputPassword){
-    const isIt = await bcrypt.compare(inputPassword,user.password);
-    if(!isIt) throw Error("Please enter Vaild info");
-    else return;
-} 
-
-module.exports = isPsswordCorrect ; 
